b72a2cd6d00d4be3d535e300a171399671e4edc6
henkegithub <135317916+henkegithub@users.noreply.github.com>
2025-12-05 13:48:45 +0100

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,2 +1,2 @@
 export const SPOTIFY_CLIENT_ID = "74e43e845cf14b71993f9dc08e8b86a2";
-export const REDIRECT_URI = "https://blind-song-scanner-azure.vercel.app/";
+export const REDIRECT_URL = "https://blind-song-scanner-azure.vercel.app/";
