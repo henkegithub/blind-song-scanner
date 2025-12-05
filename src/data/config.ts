1560a198a7861f3714a3c1d63af52d53aec632e0
henkegithub <135317916+henkegithub@users.noreply.github.com>
2025-12-05 13:39:04 +0100

    Update config.ts

--- src/data/config.ts
+++ src/data/config.ts
@@ -4,5 +4,5 @@
 export const SPOTIFY_CLIENT_ID = import.meta.env.VITE_SPOTIFY_CLIENT_ID || "<YOUR_CLIENT_ID>";
 
 // The redirect URL (can be set in env or defaults to current origin + /api/callback)
-export const REDIRECT_URL =
+export const REDIRECT_URI =
   import.meta.env.VITE_SPOTIFY_REDIRECT_URI || `${window.location.origin}/api/callback`;
